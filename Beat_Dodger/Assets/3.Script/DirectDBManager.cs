cd8ac87f1db0802439ee482483343ab339fa4aa1
OldAndSick <sps990311naver.com@gmail.com>
2026-04-03 12:01:48 +0900

    [login add]

--- Beat_Dodger/Assets/3.Script/DirectDBManager.cs
+++ Beat_Dodger/Assets/3.Script/DirectDBManager.cs
@@ -53,4 +53,56 @@ public class DirectDBManager : MonoBehaviour
             }
         });
     }
+    public void OnClickLoginButton()
+    {
+        string inputId = idInputField.text;
+        string inputPw = pwInputField.text;
+        LoginUserAsync(inputId, inputPw);
+    }
+
+    private async void LoginUserAsync(string id, string pw)
+    {
+        Debug.Log("DB에서 유저 정보 찾는 중...");
+        await Task.Run(() =>
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT PasswordHash FROM Users WHERE Username = @user";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@user", id);
+
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read()) 
+                            {
+                                string dbPassword = reader.GetString(0); 
+
+                                if (dbPassword == pw) // DB 비번 == 내가 방금 친 비번
+                                {
+                                    Debug.Log($"[로그인 성공] 🎮 {id}님, 게임 서버에 접속했습니다!");
+                                    // add move main scene
+                                }
+                                else
+                                {
+                                    Debug.LogWarning("[로그인 실패] 비밀번호가 틀렸습니다.");
+                                }
+                            }
+                            else
+                            {
+                                Debug.LogWarning("[로그인 실패] 존재하지 않는 아이디입니다.");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError("[에러 발생] DB 연결 실패: " + ex.Message);
+                }
+            }
+        });
+    }
 }
